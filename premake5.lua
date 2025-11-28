d28cb6d61f685b2886d77cf8bb188e052a8b1902
Alan Herculano Diniz <alanhdiniz@gmail.com>
2025-11-27 22:04:39 -0300

    Trying to build the project

--- premake5.lua
+++ premake5.lua
@@ -6,18 +6,37 @@ architecture "x64"
 
 project "VulkanGuide"
 kind "ConsoleApp"
+platforms { "Windows", "Linux", "Mac" }
 language "C++"
 cppdialect "C++20"
 
 targetname "VulkanGuide"
 
-files { "source/**.cpp", "source/*.cpp", "include/**.h", "include/*.h" }
+files {
+    "vendor/imgui/*.cpp",
+    "vendor/imgui/backends/imgui_impl_vulkan.cpp",
+    "vendor/imgui/backends/imgui_impl_sdl3.cpp",
+    "source/**.cpp",
+    "source/*.cpp",
+    "include/**.h",
+    "include/*.h"
+}
 
 pchheader "include/pch.h"
 
 includedirs {
-    "../vendor/sdl3/include",
     "include",
+    "vendor/imgui",
+    "vendor/imgui/backends",
+    "vendor/imgui/misc",
+    "vendor/sdl3/include",
+    "vendor/sdl3/include/SDL",
+    "vendor/stb",
+    "vendor/glm",
+    "vendor/imgui",
+    "vendor/tinyobjloader",
+    "vendor/vk-bootstrap/src",
+    "vendor/vma/include",
 }
 
 objdir "obj/%{cfg.buildcfg}/%{cfg.system}/%{cfg.architecture}/"
@@ -31,11 +50,26 @@ filter "configurations:Release"
 defines { "CONFIGURATION_RELEASE" }
 optimize "On"
 
-filter "system:windows"
+filter "configurations:Distribution"
+defines { "CONFIGURATION_DISTRIBUTION" }
+optimize "On"
+
+filter "system:Windows"
 defines { "PLATFORM_WINDOWS" }
 
-filter "system:unix"
-defines { "PLATFORM_UNIX" }
+filter "system:Linux"
+defines { "PLATFORM_LINUX" }
+libdirs {
+    "vendor/sdl3/build",
+    "vendor/glm/build/glm",
+    "vendor/tinyobjloader/build",
+    "vendor/vk-bootstrap/build",
+}
+linkoptions "-lSDL3 -lglm -ltinyobjloader -lvk-bootstrap -lvulkan"
+prebuildcommands {
+    "echo prebuildcommands\n",
+    "cp vendor/sdl3/build/libSDL3.so bin/%{cfg.buildcfg}/%{cfg.system}/%{cfg.architecture}/libSDL3.so"
+}
 
-filter "system:mac"
+filter "system:Mac"
 defines { "PLATFORM_MAC" }
--- source/main.cpp
+++ source/main.cpp
@@ -1,8 +1,7 @@
 #include "pch.h"
+#include "SDL/SDL.h"
 
 int main()
 {
-
-
     return 0;
 }
